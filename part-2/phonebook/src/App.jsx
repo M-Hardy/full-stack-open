c3cf3407389951c37fa21c0ca34a90569a9b2420
Michael <mbhardy001@gmail.com>
2024-03-12 23:57:56 -0400

    Add 2c phonebook exercise solution

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm.jsx";
 import PersonList from "./components/PersonList.jsx";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newSearch, setNewSearch] = useState("");
   const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  }, []);
 
   const handleNameChange = (event) => setNewName(event.target.value);
   const handleNumberChange = (event) => setNewNumber(event.target.value);
@@ -21,9 +23,11 @@ const App = () => {
     setNewSearch(event.target.value);
     setShowAll(newSearch === "" ? true : false);
   };
+
   const peopleToShow = showAll
     ? persons
     : persons.filter((person) => person.name.toLowerCase().includes(newSearch));
+
   const addPerson = (event) => {
     event.preventDefault();
     const personObject = {
