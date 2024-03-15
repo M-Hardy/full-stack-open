4c93a816de5c3b5c5fa2a7e8eaaabf5afda8a583
Michael <mbhardy001@gmail.com>
2024-03-15 15:38:31 -0400

    Add conditional component coloring depending on type of notification (error/entry addition).

---
+++ part-2/phonebook/src/components/Notification.jsx
@@ -0,0 +1,23 @@
+const Notification = ({ message, isError }) => {
+    const notificationColor = isError ? "red" : "green";
+    const notificationStyle = {
+        color: notificationColor,
+        background: "lightgrey",
+        fontSize: 20,
+        borderStyle: "solid",
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10,
+    };
+    if (message === null) {
+        return null;
+    }
+
+    return (
+        <div style={notificationStyle} className="notification">
+            {message}
+        </div>
+    );
+};
+
+export default Notification;
