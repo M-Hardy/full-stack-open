0dee6f2c8d03cbb974abd3c109bcc6e5e4d4cf06
Michael <mbhardy001@gmail.com>
2024-03-17 00:32:11 -0400

    Add component to display country results for exercise 3.17

--- part-2/countries/src/components/CountryResults.jsx
+++ part-2/countries/src/components/CountryResults.jsx
@@ -1,19 +1,11 @@
-import CountryInfo from "./CountryInfo";
-
-const CountryResults = ({ results }) => {
-    if (results.length === 1) {
-        return <CountryInfo countryData={results[0]} />;
-    } else if (results.length > 10) {
-        return <div>Too many matches, specify another filter</div>;
-    } else {
-        return (
-            <div>
-                {results.map((country) => (
-                    <div key={country.name.official}>{country.name.common}</div>
-                ))}
-            </div>
-        );
-    }
+const CountryResults = ({ countries }) => {
+    return (
+        <ul>
+            {countries.map((country) => {
+                <li key={country.name.official}>{country.name.common}</li>;
+            })}
+        </ul>
+    );
 };
 
 export default CountryResults;
