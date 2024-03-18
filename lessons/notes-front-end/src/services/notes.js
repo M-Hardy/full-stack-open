67028afbdb68ed7feabdc1febe85630aec4f8fa1
Michael <mbhardy001@gmail.com>
2024-03-18 14:51:27 -0400

    Add relative path to notes resources

--- lessons/notes-front-end/src/services/notes.js
+++ lessons/notes-front-end/src/services/notes.js
@@ -1,15 +1,22 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/notes";
+// Kept unchanged to show that react app can still
+// run independently from node/express web server;
+// Node/express server is just configured to serve
+// copy of react app dist as its front end (for any
+// GET request it will first check if dist directory
+// contains a file corresponding to the request's
+// address)
+
+// Relative path used for node/express copy:
+const baseUrl = "api/notes";
+// Use absolute path to see that react app still
+// works by itself
+// const baseUrl = "http://localhost:3001/api/notes";
 
 const getAll = () => {
     const request = axios.get(baseUrl);
-    const nonExisting = {
-        id: 10000,
-        content: "This note is not saved to server",
-        important: true,
-    };
-    return request.then((response) => response.data.concat(nonExisting));
+    return request.then((response) => response.data);
 };
 
 const create = (newObject) => {
