e564d0d161a54e142a3cba5023ce0c6c33bcc751
Michael <mbhardy001@gmail.com>
2024-03-23 16:52:34 -0400

    Fix totalLikes to sum 'likes' property of blog objects.

--- part-4/utils/list_helper.js
+++ part-4/utils/list_helper.js
@@ -4,7 +4,7 @@ const dummy = () => {
 
 const totalLikes = (blogs) => {
     const totalLikes = blogs.reduce(
-        (totalLikes, currentLikes) => totalLikes + currentLikes,
+        (totalLikes, currentBlog) => totalLikes + currentBlog.likes,
         0
     );
     return totalLikes;
