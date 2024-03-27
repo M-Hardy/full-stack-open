3e09de5bb874cf5884ebda94d56eb71269c53f12
Michael <mbhardy001@gmail.com>
2024-03-27 10:21:58 -0400

    Update user extractor so that if no token is received request.user defaults to 'null' instead of crashing.

--- part-4/utils/middleware.js
+++ part-4/utils/middleware.js
@@ -1,4 +1,23 @@
 const logger = require("./logger");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get("authorization");
+    if (authorization && authorization.startsWith("Bearer ")) {
+        request.token = authorization.replace("Bearer ", "");
+    }
+    next();
+};
+
+const userExtractor = async (request, response, next) => {
+    request.user = null;
+    if (request.token) {
+        const decodedToken = jwt.verify(request.token, process.env.JWT_SECRET);
+        request.user = await User.findById(decodedToken.id);
+    }
+    next();
+};
 
 const requestLogger = (request, response, next) => {
     logger.info("Method:", request.method);
@@ -26,12 +45,16 @@ const errorHandler = (error, request, response, next) => {
         return response
             .status(400)
             .json({ error: "expected `username` to be unique" });
+    } else if (error.name === "JsonWebTokenError") {
+        return response.status(400).json({ error: "token missing or invalid" });
     }
 
     next(error);
 };
 
 module.exports = {
+    tokenExtractor,
+    userExtractor,
     requestLogger,
     unknownEndpoint,
     errorHandler,
