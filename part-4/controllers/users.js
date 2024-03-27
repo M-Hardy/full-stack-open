13d7e127b5cfa3a58f1fcfcc0a745dbb250e1609
Michael <mbhardy001@gmail.com>
2024-03-27 10:33:34 -0400

    Add delete route handler and populate method to get all (see all blog content for all users).

--- part-4/controllers/users.js
+++ part-4/controllers/users.js
@@ -4,7 +4,11 @@ const bcrypt = require("bcrypt");
 
 usersRouter.get("/", async (request, response, next) => {
     try {
-        const users = await User.find({});
+        const users = await User.find({}).populate("blogs", {
+            url: 1,
+            title: 1,
+            author: 1,
+        });
         response.json(users);
     } catch (exception) {
         next(exception);
@@ -32,6 +36,15 @@ usersRouter.post("/", async (request, response, next) => {
 
         const savedUser = await newUser.save();
         response.status(201).json(savedUser);
+    } catch (exception) {
+        next(exception);
+    }
+});
+
+usersRouter.delete("/:id", async (request, response, next) => {
+    try {
+        await User.findByIdAndDelete(request.params.id);
+        response.status(204).end();
     } catch (exception) {
         next(exception);
     }
